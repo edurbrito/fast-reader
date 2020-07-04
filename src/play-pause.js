f73fcd6ce33fca09ac919f60c851990ce6ab66c3
Eduardo Brito <developer.edurbrito@gmail.com>
2020-07-04 21:55:17 +0100

    ESLint Quick Fix

--- src/play-pause.js
+++ src/play-pause.js
@@ -1,70 +1,63 @@
+/* eslint-disable no-undef */
 /** @module play-pause */
 
-var text;
-var index;
-var rate;
+var text
+var index
+var rate
 
-/** 
+/**
  * Receives a Message from the Worker
  * @event message
  */
-self.addEventListener('message' , function(e) {
-    
-    var init = false;
-    if (text == undefined)
-        init = true;
+self.addEventListener('message', function (e) {
+  var init = false
+  if (text === undefined) { init = true }
 
-    text = e.data.text;
-    index = e.data.index;
-    rate = e.data.rate;
-    if (init)
-        updateWord();
-});
+  text = e.data.text
+  index = e.data.index
+  rate = e.data.rate
+  if (init) { updateWord() }
+})
 
-var data = {};
+var data = {}
 
-/** 
+/**
  * Sends the updated data to the Worker
  * @fires message
  */
-function updateWord() {
-    var currRate = rate;
+function updateWord () {
+  var currRate = rate
 
-    if (index == text.length) {
-        postMessage(data);
-        return;
-    }
+  if (index === text.length) {
+    postMessage(data)
+    return
+  }
 
-    var word = text[index];
+  var word = text[index]
 
-    if (index > 0)
-        data.before_word = text[index - 1];
-    else
-        data.before_word = "";
-    
-    data.word = word;
-    
-    if (index < text.length - 1)
-        data.after_word = text[index + 1];
-    else
-        data.after_word = "";
-    
-    var incrment = 1;
+  if (index > 0) { data.before_word = text[index - 1] } else { data.before_word = '' }
 
-    if (word != undefined && (word.length <= 3 || word.length >= 6 || word[word.length - 1] == '.')) {
-        incrment += 1.2;
-        if (word.length >= 10)
-            incrment += 1;
-    }
+  data.word = word
 
-    currRate = rate * incrment;
+  if (index < text.length - 1) { data.after_word = text[index + 1] } else { data.after_word = '' }
 
-    index++;
-    data.index = index
+  var incrment = 1
 
-    postMessage(data);
-    if (index < text.length)
-        setTimeout( function() {
-            updateWord();
-            },currRate);
+  // if (word != undefined && (word.length <= 3 || word.length >= 6 || word[word.length - 1] == '.')) {
+  //     incrment += 1.2;
+  //     if (word.length >= 10)
+  //         incrment += 1;
+  // }
+
+  currRate = rate * incrment
+
+  index++
+  data.index = index
+
+  postMessage(data)
+  if (index < text.length) {
+    setTimeout(function () {
+      updateWord()
+    }, currRate)
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -5,17 +5,15 @@
  * @param {string} path The string path to be reduced
  * @returns {string} A new string, now shrunk
  */
-function shrinkPath(path) {
-  var newPath = new String(path);
-  
+function shrinkPath (path) {
+  var newPath = new String(path)
+
   for (var i = newPath.length; i >= 0; i--) {
-    if (newPath.charAt(i) == '/' || newPath.charAt(i) == '\\')
-      break;
+    if (newPath.charAt(i) === '/' || newPath.charAt(i) === '\\') { break }
   }
-  if(i >= 0)
-    return new String("...").concat(newPath.substring(i));
-  
-  return new String(".../").concat(newPath);
+  if (i >= 0) { return new String('...').concat(newPath.substring(i)) }
+
+  return new String('.../').concat(newPath)
 }
 
 module.exports = {
